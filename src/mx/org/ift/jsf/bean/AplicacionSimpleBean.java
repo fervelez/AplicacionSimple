401cb8af630ea1b5ced40738db13be8a2658e744
fervelez <fernando.velez.s@gmail.com>
2018-09-06 19:50:57 -0500

    Se incluye la lectura del archivo de configuración y la bitácora

---
+++ src/mx/org/ift/jsf/bean/AplicacionSimpleBean.java
@@ -0,0 +1,13 @@
+package mx.org.ift.jsf.bean;
+
+public class AplicacionSimpleBean extends AplicacionBean {
+
+	public void setNomArchConfig(String nomArchivo){
+		super.setNomArchConfig(nomArchivo);
+		
+		
+		
+        
+	}
+	
+}
